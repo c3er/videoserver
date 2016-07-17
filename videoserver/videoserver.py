cf47e5c3aa9798d26367033ac1f708ebb951aa3e
Christian Dreier <flipper.x3r@googlemail.com>
2016-07-17 04:08:38 +0200

    Use Flask

--- videoserver/videoserver.py
+++ videoserver/videoserver.py
@@ -3,47 +3,23 @@
 
 
 import os
-import enum
-import threading
 import optparse
-import http.server
+import flask     # pip install Flask
 
 
 DEFAULT_PORT = 3030
 
 
-# HTTP related #################################################################
-
-class HTTPStatus(enum.Enum):
-    ok = 200
-    notfound = 404
+app = flask.Flask(__name__)
 
 
-class VideoRequestHandler(http.server.BaseHTTPRequestHandler):
-    def _send_normalheader(self):
-        self.send_response(HTTPStatus.ok.value)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
 
-    def do_HEAD(self):
-        self._send_normalheader()
+# Handlers #####################################################################
 
-    def do_GET(self):
-        self._send_normalheader()
-        self.wfile.write(bytes('<html><body><h1>Hello web server</h1></body></html>', "utf8"))
+@app.route("/")
+def hello():
+    return '<html><body><h1>Hello web server</h1></body></html>'
 
-
-class HTTPServerWorker(threading.Thread):
-    def __init__(self, port, root_path):
-        super().__init__()
-        self.httpd = http.server.HTTPServer(("", port), VideoRequestHandler)
-        
-    def run(self):
-        self.httpd.serve_forever()
-    
-    def shutdown(self):
-        self.httpd.shutdown()
-        
 ################################################################################
 
 
@@ -56,8 +32,11 @@ def get_option_count(options):
             opt_count += 1
     return opt_count
 
+################################################################################
 
-def parse_args():
+
+def main():
+    # Evaluate given options ###################################################
     parser = optparse.OptionParser()
     parser.add_option(
         '-p',
@@ -71,15 +50,8 @@ def parse_args():
         dest='root_path',
         help='Root path. The default root path is the current directory.'
     )
-    options, args = parser.parse_args()  # "args" not needed here
-    return parser, options
+    options, args = parser.parse_args()  # "args" not needed
 
-################################################################################
-
-
-def main():
-    # Evaluate given options ###################################################
-    parser, options = parse_args()
     if get_option_count(options) == 0:
         parser.print_help()
     
@@ -101,21 +73,9 @@ def main():
     else:
         root_path = ''
     ############################################################################
-    
-    httpd = HTTPServerWorker(port, root_path)
-    
-    print(
-        'Serving at port {} with root path "{}"'.format(port, root_path),
-        'http://localhost:{}/'.format(port),
-        'Press Enter to exit.',
-        sep='\n'
-    )
-    httpd.start()
-    
-    input()
-    print('Exiting')
-    httpd.shutdown()
-    
-    
-if __name__ == '__main__':
+
+    app.run(port=port)
+
+
+if __name__ == "__main__":
     main()
