91ae3e9d146fbf1d1f201ec97f6829f461c38795
Christian Dreier <flipper.x3r@googlemail.com>
2017-09-03 23:17:59 +0200

    Simplify video streaming

--- videoserver/app/pages/filecontent.py
+++ videoserver/app/pages/filecontent.py
@@ -8,19 +8,6 @@ import app
 import app.fs
 
 
-BLOCKSIZE = 1024
-
-
 @app.servicehandler(app.services.filecontent)
 def get_filecontent(path):
-    file = app.fs.getfile(path)
-
-    # See http://flask.pocoo.org/docs/0.12/patterns/streaming/
-    def generate():
-        with open(file.ospath, "rb") as f:
-            while True:
-                data = f.read(BLOCKSIZE)
-                if not data:
-                    break
-                yield data
-    return flask.Response(generate(), mimetype='video/mp4')
+    return flask.send_file(app.fs.getfile(path).ospath, mimetype="video/mp4")
